20e1e45b744671cb3b8a89a2b67a5565e9558db4
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-01-28 22:27:31 +0100

    typing in bayesian layer

--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,10 @@ from torch.distributions.normal import Normal
 
 
 class BayesianLayer(nn.Module):
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features: int, out_features: int) -> None:
         super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
+        self.in_features: int = in_features
+        self.out_features: int = out_features
         self.mu: nn.Parameter = nn.Parameter(torch.randn(in_features, out_features))
         self.ro: nn.Parameter = nn.Parameter(torch.randn(in_features, out_features))
 
@@ -16,16 +16,18 @@ class BayesianLayer(nn.Module):
 
         self.normal: Normal = Normal(0, 1)
 
-    def forward(self, x):
-        batch_size = x.shape[0]
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        batch_size: int = x.shape[0]
 
-        eps = self.normal.rsample((batch_size, self.in_features, self.out_features))
-        eps_bias = self.normal.rsample((batch_size, self.out_features))
+        eps: torch.Tensor = self.normal.rsample(
+            (batch_size, self.in_features, self.out_features)
+        )
+        eps_bias: torch.Tensor = self.normal.rsample((batch_size, self.out_features))
 
-        sigma = torch.log(1 + torch.exp(self.ro))
-        sigma_bias = torch.log(1 + torch.exp(self.ro_bias))
+        sigma: torch.Tensor = torch.log(1 + torch.exp(self.ro))
+        sigma_bias: torch.Tensor = torch.log(1 + torch.exp(self.ro_bias))
 
-        weights = eps * sigma + self.mu
-        bias = eps_bias * sigma_bias + self.mu_bias
+        weights: torch.Tensor = eps * sigma + self.mu
+        bias: torch.Tensor = eps_bias * sigma_bias + self.mu_bias
 
         return (x.unsqueeze(1) @ weights).squeeze() + bias
